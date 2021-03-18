f231f3822dddea3c6618924d37e383f5ba48a074
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-03-18 09:41:09 +0530

    Add files via upload

---
+++ alpha1.py
@@ -0,0 +1,8 @@
+ch=65
+n=eval(input("Enter the number of lines"))
+for i in range(1,n+1):
+	for j in range (1,i+1):
+		print(chr(ch),end=" ")
+		ch+=1
+	print()
+
---
+++ alpha2.py
@@ -0,0 +1,8 @@
+n=eval(input("Enter the number of lines"))
+for i in range(1,n+1):
+	
+	for j in range (1,i+1):
+		print("*",end=" ")
+		n+=1
+	print()
+
