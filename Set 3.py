3eabd88436ce3d365bf86ae3fdf72a712b22a1c9
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-05-22 20:07:55 +0530

    Difference

---
+++ Set 3.py
@@ -0,0 +1,10 @@
+s1={100,200, 300, 400, 500}
+s2={400, 500, 600, 700, 800,900}
+#function
+print(s1.difference(s2))
+#Symbol
+print(s1-s2)
+#function
+print(s2.difference(s1))
+#Symbol
+print(s2-s1)
