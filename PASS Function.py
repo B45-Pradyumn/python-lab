c31396946a5f11948b04bcf9fc06dc677d8838bd
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-12 01:48:43 +0530

    Add files via upload

---
+++ PASS Function.py
@@ -0,0 +1,27 @@
+#FUNCTION FOR PASS STATEMENT
+
+def fun1():
+    pass
+
+
+def fun2():
+    pass
+
+
+def fun3():
+    print("Hello")
+fun3()
+
+
+def fun4():
+    pass
+
+
+def fun5():
+    print("GLA")
+fun5()
+
+
+def fun6(a,b):
+    print(a+b)
+fun6(3,7)
