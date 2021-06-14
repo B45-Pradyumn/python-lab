61687f98323412aa2dc486e672ae3fe884f27c02
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-15 01:17:54 +0530

    Add files via upload

---
+++ Check Randint fun.py
@@ -0,0 +1,10 @@
+#CHECK RANDINT FUNCTION
+
+import random
+while True:
+    number=random.randint(1,20)
+    if number==6:
+        print("You can open your 1 player")
+
+                                 
+                                 
