f6c7d47860bba697f07576711900784da7333409
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-15 01:18:21 +0530

    Add files via upload

---
+++ Randrange fun.py
@@ -0,0 +1,10 @@
+#RANDRANGE FUNCTION
+
+import random
+print(random.randrange(2,20,2))
+print(random.randrange(1,100,5))
+print(random.randrange(1,100,5))
+print(random.randrange(2,20))
+
+                                 
+                                 
