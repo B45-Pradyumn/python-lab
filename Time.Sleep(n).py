80aaf2eec050a9c03feb1f940befa066163b1fcf
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-18 02:41:07 +0530

    Gives the time for output

---
+++ Time.Sleep(n).py
@@ -0,0 +1,13 @@
+#TIME.SLEEP(n)
+
+import time
+for i in "Python Programming":
+    print (i,end=' ')
+    time.sleep(0.6)            # n= 0.7 is no.of seconds
+print('
+      --------------------------------------------')    
+for a in range(1,21):
+    print(a)
+    time.sleep(0.4)
+
+
