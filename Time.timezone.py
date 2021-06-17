bc713f7f0efd7e05534cc1759018b51016c11c4d
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-18 02:40:05 +0530

    Difference b/w Current time & GST

---
+++ Time.timezone.py
@@ -0,0 +1,9 @@
+# TIME.TIMEZONE:-     it is a attribute no a fun
+
+import time
+a=time.timezone
+print(a)
+print("---------------------------------------------")
+b=time.time()
+print(b)          # it shows tge no.of sec. since 1 Jan.1970
+
