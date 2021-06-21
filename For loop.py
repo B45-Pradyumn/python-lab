afec4fc417f68c76f8963a43bd5223ee0f73ecb0
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-21 23:52:51 +0530

    Function

---
+++ For loop.py
@@ -0,0 +1,16 @@
+#FOR LOOP
+for i in range(55,150,5):
+    print(i)
+for a in range(20,2,-3):
+    print(a)
+
+    # For even no.
+for i in range(0,100,2):
+    print(i)
+
+    #For odd no.
+for i in range(1,100,2):
+    print(i)
+    
+    
+
