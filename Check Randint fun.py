65a91badb8eabb4b44a6295f11781104f3001c66
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-22 01:35:35 +0530

    Update Check Randint fun.py

--- Check Randint fun.py
+++ Check Randint fun.py
@@ -5,6 +5,10 @@ while True:
     number=random.randint(1,20)
     if number==6:
         print("You can open your 1 player")
+        break
+    else:
+        print("You lost the game")
+        break
 
                                  
                                  
