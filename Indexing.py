d343ec1e11fb549c7e0e36de2c820e4f2341b871
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-21 23:52:16 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,13 @@
+a="Hello World"
+print(a[4])
+print(a[0])
+print(a[-4])
+print(a[1:4])
+print(a[10])
+print(a[-6])
+print(a[1:10:2])
+print(a[1:10:20])
+print(a[10:1:-1])
+print(a[::2])
+print(a[::-1])
+print(a[12:20])
