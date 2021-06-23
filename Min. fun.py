2634f440ac98719ecbd1772a1c6b8e1795ee88d6
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-06-23 23:48:05 +0530

    Add files via upload

---
+++ Min. fun.py
@@ -0,0 +1,9 @@
+#Min.FUN
+L=[3,4,5,6]
+M=[5,8]
+print(min(L)>min(M))        #max fun only checks the last value
+print(min(L)<max(M))        
+print(min(L)<min(M))        #min fun only checks the 1st value
+print(min(L)>max(M))
+                            
+
