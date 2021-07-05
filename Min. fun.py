7a4304ffda3a8b76fea3a11bd61760db703aea88
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-07-05 23:59:35 +0530

    Update Min. fun.py

--- Min. fun.py
+++ Min. fun.py
@@ -5,5 +5,4 @@ print(min(L)>min(M))        #max fun only checks the last value
 print(min(L)<max(M))        
 print(min(L)<min(M))        #min fun only checks the 1st value
 print(min(L)>max(M))
-                            
-
+                         
