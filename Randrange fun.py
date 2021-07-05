2cb2f4b36a89ac197107883a1db47efbe1c5fb1c
Pradyumn Gupta <80021291+B45-Pradyumn@users.noreply.github.com>
2021-07-05 23:58:57 +0530

    Update Randrange fun.py

--- Randrange fun.py
+++ Randrange fun.py
@@ -4,7 +4,4 @@ import random
 print(random.randrange(2,20,2))
 print(random.randrange(1,100,5))
 print(random.randrange(1,100,5))
-print(random.randrange(2,20))
-
-                                 
-                                 
+print(random.randrange(2,20))                   
